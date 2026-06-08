542fadadd9be689e3ce086c3d141d6f1276d5b20
Sergei Mahnev <s.mahnev@skillline-tech.ru>
2026-06-08 17:21:56 +0200

    Правки

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // В dev картинки и так без агрессивного кропа; на проде отключаем /_next/image
+  images: {
+    unoptimized: process.env.NODE_ENV === "production",
+  },
+  allowedDevOrigins: ["l4xkq4-92-101-127-10.ru.tuna.am"],
 };
-
-module.exports = {
-  allowedDevOrigins: ['l4xkq4-92-101-127-10.ru.tuna.am'],
-}
 
 export default nextConfig;
